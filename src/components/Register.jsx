40364ac8f2b1b33645529b0d72092a037461f693
Kushal Bajracharya <bajrakus20@gmail.com>
2023-08-11 21:17:49 +0545

    Added: frontend part

---
+++ src/components/Register.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
+import { Link, Navigate,useNavigate } from 'react-router-dom';
+import { AuthContext } from '../AuthProvider';
+
+function Register() {
+    const { isLoggedIn, logIn } = useContext(AuthContext);
+    const navigate = useNavigate();
+    // check if user is already logged in or has a token in local storage
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            logIn();
+        }
+    }, [logIn]);
+
+    // if user is already logged in, navigate them to the home page
+    if (isLoggedIn) {
+        return <Navigate to="/" replace />;
+    }
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const register = () => {
+        axios.post('http://localhost:3000/api/user/register', { username, password })
+            .then(res => {
+                navigate('/login');
+            });
+    };
+
+    return (
+        <div>
+            <h1>Register</h1>
+            <input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+            <button onClick={register}>Register</button>
+            <p>Already have an account? <Link to="/login">Login</Link></p>
+        </div>
+    );
+}
+
+export default Register;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { AuthContext } from '../AuthProvider';
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [username, setUsername] = useState("");
+    const [newTodo, setNewTodo] = useState("");
+    const { isLoggedIn, logOut } = useContext(AuthContext);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:3000/api/todos/getusertodo', { headers: { Authorization: `Bearer ${token}` } })
+            .then(res => {
+                setTodos(res.data);
+            });
+    }, []);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:3000/api/user/username', { headers: { Authorization: `Bearer ${token}` } })
+            .then(res => {
+                setUsername(res.data.username);
+            });
+    }, []);
+
+    const addTodo = () => {
+        const token = localStorage.getItem('token');
+        axios.post('http://localhost:3000/api/todos/createusertodo', { content: newTodo }, { headers: { Authorization: `Bearer ${token}` } })
+            .then(res => {
+                setTodos(prevTodos => [...prevTodos, res.data]);
+                setNewTodo("");
+            });
+    };
+
+    const deleteTodo = (id) => {
+        const token = localStorage.getItem('token');
+        axios.delete(`http://localhost:3000/api/todos/deleteusertodo/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+            .then(res => {
+                setTodos(prevTodos => prevTodos.filter(todo => todo._id !== id));
+            })
+            .catch(err=>{
+                console.error("Error deleting todo:",err);
+            });
+    };
+
+    return (
+        <div>
+            <h2>Welcome {username}</h2>
+            <nav>{isLoggedIn && <button onClick={logOut}>Log Out</button>}</nav>
+            <h1>Todos</h1>
+            <input value={newTodo} onChange={e => setNewTodo(e.target.value)} />
+            <button onClick={addTodo}>Create</button>
+            {todos.map((todo, index) => (
+                <div key={index}>
+                    <p>{todo.content}</p>
+                    <p>Created at - {new Date(todo.timestamp).toLocaleDateString()}</p>
+                    <button><Link to={`/update/${todo._id}`}>Update</Link></button>
+                    <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+export default TodoList;
