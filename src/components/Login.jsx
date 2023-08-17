dd269373ac30dd0a3a484c76463f95e5f5927612
smriteemanandhar <smriteemanandhar@gmail.com>
2023-08-17 20:38:34 +0545

    Added: html and css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import { Link, Navigate } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider';
+import'./Login.css';
 
 function Login() {
     const { isLoggedIn, logIn } = useContext(AuthContext);
@@ -31,14 +32,35 @@ function Login() {
     };
 
     return (
-        <div>
+       <div className="contain">
+            <h1 className = "heading">Login</h1>
+            <div className = "form">
+            <div className="txt_field">
+                <input type="text" required placeholder="Username" onChange={e => setUsername(e.target.value)}/>
+                <span></span>
+                <label>Username</label>
+            </div>
+            <div className="txt_field">
+                <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                <span></span>
+                <label>Password</label>
+            </div>
+            <div className="pass">Forgot Password?</div>
+            <button className = "button" onClick={login}>Login</button>
+            <div className="signup_link">
+                <p>Don't have an account? <Link to="/register">Register</Link></p>
+            </div>
+            </div>
+        </div>
+        /* <div class = "center">
             <h1>Login</h1>
             <input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
             <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
             <button onClick={login}>Login</button>
             <p>Don't have an account? <Link to="/register">Register</Link></p>
         </div>
+        */
     );
 }
-
 export default Login;
+
